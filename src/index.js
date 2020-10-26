570099de6bab87a0f187d35d8317f3127a54515f
VO THANH PHONG <vophong1612@gmail.com>
2020-10-26 21:22:38 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import Pricing from './component/board/board.js'
+
+function App() {
+    return (
+        <div>
+            <Pricing />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
